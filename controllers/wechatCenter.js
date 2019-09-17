dd7cf62b2fc7fcbbbce8e0674aef9871449923a1
herrylo <herryloyopai@163.com>
2019-09-17 23:53:17 +0800

    add

--- controllers/wechatCenter.js
+++ controllers/wechatCenter.js
@@ -1,5 +1,9 @@
 module.exports = {
     async get(ctx, next) {
         await next();
+    },
+    
+    async post(ctx, next) {
+        await next();
     }
 }
--- router/router.js
+++ router/router.js
@@ -6,4 +6,6 @@ const wechatCenter = require('../controllers/wechatCenter');
 // controller/wechatCenter
 Routers.get('/wechat', wechatCenter.get);
 
+Routers.post('/wechat', wechatCenter.post);
+
 module.exports = Routers
