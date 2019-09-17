a425c7b0178d3ef556a254908c215a08b80ce6d9
herrylo <herryloyopai@163.com>
2019-09-18 00:22:33 +0800

    set random

--- middleware/wechat.js
+++ middleware/wechat.js
@@ -8,12 +8,13 @@ module.exports = wechat(config.wechat).middleware(
                 type: 'text',
                 content: '~hi!!谢谢关注Yopai的公众号，每周不定期更新，技术博客：https://didiheng.com/，当然这里不光有技术分享，还有其他哟。'
             },
-            {
+            [
+                {
                 title: '欢迎交流哦！！😁😁',
                 description: '',
                 picurl: 'https://didiheng.com/WechatIMG199.jpeg',
                 url: ''
-            }
+            }]
         ]
         let index = Math.round(Math.random())
         return array[index];
