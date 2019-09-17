5ee79425dbf5e3f0d8619b03963fc75464a832e4
herrylo <herryloyopai@163.com>
2019-09-17 23:15:41 +0800

    add

--- config/index.js
+++ config/index.js
@@ -4,6 +4,6 @@ module.exports = {
         token : 'weixin',
         appid : 'wxd10f8cbcf28c61b1',
         appsecret :'7ce0b6983abe6001f2efe4f7c62e6f80',
-        encodingAESKey : ''
+        encodingAESKey : '7ce0b6983abe6001f2efe4f7c62e6f80'
     }
 }
--- controllers/wechatCenter.js
+++ controllers/wechatCenter.js
@@ -1,18 +1,13 @@
-const sha1 = require('sha1');
 const config = require('../config');
+const wechat = require('co-wechat');
 
 function wechatCenter() {
     this.wechat = async (ctx, next) => {
-        console.log('query参数：', ctx.query)
-        let token = config.wechat.token;
-        let signature = ctx.query.signature;
-        let nonce = ctx.query.nonce;
-        let timestamp = ctx.query.timestamp;
-        let echostr = ctx.query.echostr;
-        let str = [token,timestamp,nonce].sort().join(''); //按字典排序，拼接字符串
-        let sha = sha1(str); //加密
-        console.log('signature和sha比较：', sha === signature)
-        ctx.body = (sha === signature)? echostr + '' : 'failed';  //比较并返回结果
+        wechat(config).middleware(async (message, ctx) => {
+            if (message.FromUserName === '1') {
+                return 'hehe';
+            }
+        })
     }
 }
 
